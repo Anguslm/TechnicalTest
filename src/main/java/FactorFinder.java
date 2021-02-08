7079142ff3882e87896b22d017f6f9b2a1eb91fd
Anguslm <amacdougall@spartaglobal.com>
2021-02-08 17:30:42 +0000

    Wrote factorisation method

---
+++ src/main/java/FactorFinder.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FactorFinder {
+    public String convert(int input) {
+        HashMap<Integer, String> factorMap = new HashMap<Integer, String>();
+        factorMap.put(3, "Pling");
+        factorMap.put(5, "Plang");
+        factorMap.put(7, "Plong");
+        StringBuilder outputString = new StringBuilder();
+        for (Map.Entry<Integer, String> entry: factorMap.entrySet()) {
+            if (input % entry.getKey() == 0) {
+                outputString.append(entry.getValue());
+            }
+        }
+        if (outputString.length() == 0) {
+            outputString.append(input);
+        }
+        return outputString.toString();
+    }
+
+    public static void main(String[] args) {
+        FactorFinder f = new FactorFinder();
+        System.out.println(f.convert(0));
+    }
+}
